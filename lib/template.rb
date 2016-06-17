e428f1175ff1e12e91ace4a193cb58ef179c990a
Caleb Woods <calebawoods@gmail.com>
2016-06-17 15:32:53 -0400

    Adds a basic Docker setup needed for a standard Rails app on PG
    Tested by generating a Sample app and then booting with Docker.
    
    ```
    $ rails new sample -m Rails-Template/lib/template.rb
    $ cd sample
    $ docker-compose up
    ```

--- lib/template.rb
+++ lib/template.rb
@@ -5,11 +5,13 @@ templates_root = File.expand_path(File.join('..', 'templates'), File.dirname(__F
 source_paths << templates_root
 
 module RMSTemplate
-  RUBY_VERSION = '2.3.0'.freeze
+  RUBY_VERSION = '2.3.1'.freeze
   FILES = %w(
     .rubocop.yml
     Gemfile.tt
     .ruby-version.tt
+    docker-compose.yml.tt
+    Dockerfile.tt
   )
 end
 
@@ -25,8 +27,12 @@ end
 
 # Set up test frameworks
 after_bundle do
-  generate 'rspec:install'
-  generate 'spinach'
+  if @use_docker
+    puts 'Cannot automatically run generators with Docker'
+  else
+    generate 'rspec:install'
+    generate 'spinach'
+  end
 end
 
 # Set up Git Repo, if desired
@@ -43,6 +49,13 @@ RMSTemplate::FILES.each do |filename|
   template(filename, force: force_our_files)
 end
 
+if (@use_docker = yes?('Set up Docker?'))
+  def run_bundle
+    # Want to run bundler in the Docker container rather local system
+    # However we need a stub Gemfile.lock for the container to pickup
+    file 'Gemfile.lock', ''
+  end
+end
 
 # Since we have a .ruby-version file being added ... if we don't match,
 # bundle install will fail (or install on the wrong Ruby version). So
